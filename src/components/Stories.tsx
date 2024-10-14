8f9dbc7eeec22cb21ad5734e962992d0cf042449
junting <342766475@qq.com>
2024-10-14 22:22:35 +0800

    feat: content - Stories

---
+++ src/components/Stories.tsx
@@ -0,0 +1,144 @@
+import Image from 'next/image'
+
+const Stories = () => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
+      <div className="flex gap-8 w-max">
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/users/avatars/807850392/a-shotara-367.jpeg?auto=compress&fit=crop&h=130&w=130&dpr=2"
+            alt=""
+            width={20}
+            height={20}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/users/avatars/807850392/a-shotara-367.jpeg?auto=compress&fit=crop&h=130&w=130&dpr=2"
+            alt=""
+            width={20}
+            height={20}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/users/avatars/807850392/a-shotara-367.jpeg?auto=compress&fit=crop&h=130&w=130&dpr=2"
+            alt=""
+            width={20}
+            height={20}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/users/avatars/807850392/a-shotara-367.jpeg?auto=compress&fit=crop&h=130&w=130&dpr=2"
+            alt=""
+            width={20}
+            height={20}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/users/avatars/807850392/a-shotara-367.jpeg?auto=compress&fit=crop&h=130&w=130&dpr=2"
+            alt=""
+            width={20}
+            height={20}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/users/avatars/807850392/a-shotara-367.jpeg?auto=compress&fit=crop&h=130&w=130&dpr=2"
+            alt=""
+            width={20}
+            height={20}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/users/avatars/807850392/a-shotara-367.jpeg?auto=compress&fit=crop&h=130&w=130&dpr=2"
+            alt=""
+            width={20}
+            height={20}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/users/avatars/807850392/a-shotara-367.jpeg?auto=compress&fit=crop&h=130&w=130&dpr=2"
+            alt=""
+            width={20}
+            height={20}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/users/avatars/807850392/a-shotara-367.jpeg?auto=compress&fit=crop&h=130&w=130&dpr=2"
+            alt=""
+            width={20}
+            height={20}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/users/avatars/807850392/a-shotara-367.jpeg?auto=compress&fit=crop&h=130&w=130&dpr=2"
+            alt=""
+            width={20}
+            height={20}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/users/avatars/807850392/a-shotara-367.jpeg?auto=compress&fit=crop&h=130&w=130&dpr=2"
+            alt=""
+            width={20}
+            height={20}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+        {/* Story */}
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/users/avatars/807850392/a-shotara-367.jpeg?auto=compress&fit=crop&h=130&w=130&dpr=2"
+            alt=""
+            width={20}
+            height={20}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">Ricky</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Stories
