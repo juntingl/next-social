b473dd1a1693b40bdc8c0e99c5bfb34db4749692
junting <342766475@qq.com>
2024-10-14 21:48:50 +0800

    feat: navbar - search

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import Link from "next/link"
 import { IoIosPeople, IoMdNotificationsOutline } from "react-icons/io"
-import { IoHomeOutline } from "react-icons/io5"
+import { IoHomeOutline, IoSearch } from "react-icons/io5"
 import { MdOutlineHistory } from "react-icons/md"
 import MobileMenu from "./MobileMenu"
 import { GoPeople } from "react-icons/go"
@@ -16,21 +16,29 @@ const Navbar = () => {
         <Link href="/" className="font-bold text-xl text-blue-600 ">NextSocial</Link>
       </div>
       {/* Center */}
-      <div className="hidden md:flex w-[50%]">
+      <div className="hidden md:flex w-[50%] items-center justify-between">
         {/* Links */}
         <div className="flex gap-6 text-gray-600">
           <Link href="/" className="flex gap-2 items-center">
-            <IoHomeOutline width={16} height={16} className="w-4 h-4" />
+            <IoHomeOutline width={24} height={24} className="w-6 h-6" />
             <span>Home</span>
           </Link>
           <Link href="/" className="flex gap-2 items-center">
-            <IoIosPeople width={16} height={16} className="w-4 h-4" />
+            <IoIosPeople width={24} height={24} className="w-6 h-6" />
             <span>Friends</span>
           </Link>
           <Link href="/" className="flex gap-2 items-center">
-            <MdOutlineHistory width={16} height={16} className="w-4 h-4" />
+            <MdOutlineHistory width={24} height={24} className="w-6 h-6" />
             <span>Stories</span>
           </Link>
+        </div>
+        <div className="hidden xl:flex p-2 bg-slate-100 items-center rounded-md">
+          <input
+            type="text"
+            placeholder="search..."
+            className="bg-transparent outline-none"
+          />
+          <IoSearch width={14} height={14} />
         </div>
       </div>
       {/* Right */}
