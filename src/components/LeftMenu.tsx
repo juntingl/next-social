4fef9724d1716b7ec4ebb8e1fcd8e9e9fa6dff66
Junting <342766475@qq.com>
2024-10-15 21:26:54 +0800

    feat: leftMenu

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -1,7 +1,21 @@
-const LeftMenu = () => {
+import Link from "next/link"
+import Image from "next/image"
+
+const LeftMenu = ({ type }: { type: "home" | "profile" }) => {
   return (
     <div className="flex flex-col gap-6">
+      {/* Profile */}
 
+      <div className="p-4 bg-white rounded-lg shadow-md text-sm text-gray-500 flex flex-col gap-2">
+        <Link href="/">
+          <Image
+            src="/posts.png"
+            alt=""
+            width={20}
+            height={20}
+          />
+        </Link>
+      </div>
     </div>
   )
 }
